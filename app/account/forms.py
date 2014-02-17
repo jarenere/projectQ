eebadd1313856e6d152ad95cdaab88d2a5319621
nukru <nu_kru@hotmail.com>
2014-02-17 15:06:39 +0100

    added showQuestion.html that render a section dynamically (wtforms dynamic)

--- app/account/forms.py
+++ app/account/forms.py
@@ -1,7 +1,22 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
+from wtforms import TextField, BooleanField, RadioField, IntegerField
 from wtforms.validators import Required
 
 class LoginForm(Form):
     openid = TextField('openid', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
+
+class AnswerNumericalForm(Form):
+    answer = IntegerField('Answer')
+
+class AnswerYNForm(Form):
+    answer = RadioField('Answer', choices = [('Yes','Yes'),('No','No')])
+
+class AnswerTextForm(Form):
+    answer = TextField('Answer')
+
+class AnswerChoiceForm(Form):
+    answer = RadioField('Answer')
+
+class Answer(Form):
+        pass
--- app/account/views.py
+++ app/account/views.py
@@ -3,11 +3,17 @@ from app import app, db, lm, oid
 from flask import Blueprint, request, url_for, flash, redirect, abort, session, g
 from flask import render_template
 from flask.ext.login import login_user, logout_user, current_user, login_required
-from forms import LoginForm
+from forms import LoginForm, AnswerChoiceForm, AnswerNumericalForm, AnswerTextForm, AnswerYNForm
+from forms import Answer
 from app.models import Survey, Consent, Section
 from app.models import Question, QuestionChoice, QuestionNumerical, QuestionText
 from app.models import QuestionYN
 from app.models import StateSurvey
+
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField, RadioField, IntegerField
+from wtforms.validators import Required
+
 
 blueprint = Blueprint('account', __name__)
 
@@ -56,8 +62,9 @@ def logicSurvey(id_survey):
     stateSurvey = StateSurvey.getStateSurvey(id_survey,g.user)
     if (stateSurvey.consented == False):
         return redirect(url_for('account.showConsent', id_survey = id_survey))
-    
-    return redirect (url_for('account.index'))
+    section = Survey.query.get(id_survey).sections.first()
+    return redirect (url_for('account.showQuestions',id_survey=id_survey,id_section=section.id))
+    # return redirect (url_for('account.index'))
 
 @blueprint.route('/survey/<int:id_survey>/consent', methods=['GET', 'POST'])
 @login_required
@@ -77,12 +84,59 @@ def showConsent(id_survey):
         survey = survey,
         consent = survey.consents.first())
 
+class AnswerRender():
+    #: Type contains the type of question
+    type = None
+    #: form, the form of question
+    form = None
+    #: text of question
+    text = None
+    def __init__(self, type=None, form=None, text=None):
+        self.type = type
+        self.form = form
+        self.text = text
+
+
+
 @blueprint.route('/survey/<int:id_survey>/section/<int:id_section>', methods=['GET', 'POST'])
 @login_required
 def showQuestions(id_survey, id_section):
     '''
     Show all question of a section
     '''
+    survey = Survey.query.get(id_survey)
+    section = Section.query.get(id_section)
+    questions = section.questions
+    # Answer.prueba = TextField('Prueba')
+    for question in questions:
+        if isinstance (question,QuestionYN):
+            setattr(Answer,"c"+str(question.id),RadioField('Answer', choices = [('Yes','Yes'),('No','No')]))
+            # ans = AnswerRender(type='YN', text = question.text)
+            # list.append(ans)
+        if isinstance (question,QuestionNumerical):
+            setattr(Answer,"c"+str(question.id),IntegerField('Answer'))
+            # ans = AnswerRender(type='Numerical', text = question.text)
+            # list.append(ans)
+        if isinstance (question,QuestionText,):
+            setattr(Answer,"c"+str(question.id),TextField('Answer'))
+            # ans = AnswerRender(type='Text', text = question.text)
+            # list.append(ans)
+        # if isinstance (question,QuestionChoice):
+        #     ans = AnswerRender(type='Choice',form = AnswerChoiceForm(), text = question.text)
+        #     ans.form.answer.choice = question.choices
+        #     list.append = ans
+
+    form = Answer()
+    return render_template('/account/showQuestions.html',
+            title = survey.title,
+            survey = survey,
+            section = section,
+            form = form,
+            questions = questions)
+
+
+
+
     
 
 
