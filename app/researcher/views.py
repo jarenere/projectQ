07d5407f8cf16bae3557f4ea14dfb025323f3cf2
nukru <nu_kru@hotmail.com>
2014-03-25 17:01:16 +0100

    fixed add/edit question render(before disable question type)

--- app/researcher/views.py
+++ app/researcher/views.py
@@ -439,8 +439,8 @@ def addQuestion(id_survey, id_section):
         section = section,
         questions = section.questions,
         addQuestion = True,
-        question_type = form.questionType.data,
-        decision_type = form.decisionType.data,
+        question_type = "yn",
+        decision_type = "none",
         path = path)
 
 
