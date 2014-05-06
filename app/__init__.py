202bff1d3d37b211b5ff2cbc1acc2f46d2d2fccc
nukru <nu_kru@hotmail.com>
2014-05-06 11:33:13 +0200

    reorganizate files

--- app/__init__.py
+++ app/__init__.py
@@ -113,7 +113,7 @@ app.jinja_env.globals['status_part3'] = status_part3
 if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/swarms.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler = RotatingFileHandler('temp/swarms.log', 'a', 1 * 1024 * 1024, 10)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.addHandler(file_handler)
