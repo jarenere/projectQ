4fc37defc51510caab8c685d3db74c70dc6c99e1
nukru <nu_kru@hotmail.com>
2014-07-03 00:03:50 +0200

    update

--- app/auth/views.py
+++ app/auth/views.py
@@ -63,14 +63,13 @@ def register():
     if form.validate_on_submit():
         user = User(email=form.email.data,
                     password=form.password.data)
-        print form.password.data
         db.session.add(user)
         db.session.commit()
         # token = user.generate_confirmation_token()
         # send_email(user.email, 'Confirm Your Account',
         #            'auth/email/confirm', user=user, token=token)
         # flash(gettext('A password has been sent to you by email.'))
-        return redirect(url_for('auth.login'))
+        return redirect(url_for('auth.login_email'))
     return render_template('auth/register.html', form=form)
 
 
